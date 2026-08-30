a570eb496a4492fff859ff1843a88815fba4e72e
abot8639 <abot8639@gmail.com>
2026-08-30 18:52:02 +0300

    docs: update README with banner and simplified documentation and import flutter/services in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:blob_flutter/blob_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const ParticleBlobExampleApp());
