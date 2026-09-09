1f70f23423fad6a155011b7bb8ed583d8b128266
abot8639 <abot8639@gmail.com>
2026-09-09 05:45:19 +0300

    test: update widget test to match renamed app class and showcase page

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:example/main.dart';
 
 void main() {
-  testWidgets('ParticleBlobExampleApp smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ParticleBlobExampleApp());
-    expect(find.byType(DashboardPage), findsOneWidget);
+  testWidgets('BlobExampleApp smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const BlobExampleApp());
+    expect(find.byType(BlobShowcasePage), findsOneWidget);
   });
 }
