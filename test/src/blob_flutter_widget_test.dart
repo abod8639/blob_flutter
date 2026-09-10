a503952d1df0aa620631b4e5fcf2d9c1a217bf76
abot8639 <abot8639@gmail.com>
2026-09-11 01:39:38 +0300

    refactor: remove unused flutter/services import in blob_flutter_widget_test.dart

--- test/src/blob_flutter_widget_test.dart
+++ test/src/blob_flutter_widget_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:blob_flutter/src/blob_controller.dart';
 import 'package:blob_flutter/src/blob_flutter_widget.dart';
