c925d669bbdb2bcab2086aba09b6604142245ba6
abot8639 <abot8639@gmail.com>
2026-09-11 02:44:44 +0300

    docs: add library documentation and reorder export statements in blob_flutter.dart

--- lib/blob_flutter.dart
+++ lib/blob_flutter.dart
@@ -1,5 +1,8 @@
-export 'src/blob_flutter_widget.dart';
+/// A high-performance interactive 3D particle blob effect for Flutter.
+library blob_flutter;
+
 export 'src/blob_controller.dart';
-export 'src/blob_noise_type.dart';
 export 'src/blob_exception.dart';
+export 'src/blob_flutter_widget.dart';
+export 'src/blob_noise_type.dart';
 
