4470d06e96ac1568b458c3e33364b966fa734b68
abot8639 <abot8639@gmail.com>
2026-09-16 01:57:35 +0300

    test: import blob_compute_params in widget tests

--- test/src/blob_flutter_widget_test.dart
+++ test/src/blob_flutter_widget_test.dart
@@ -1,4 +1,5 @@
 import 'dart:typed_data';
+import 'package:blob_flutter/src/blob_compute_params.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:blob_flutter/src/blob_controller.dart';
